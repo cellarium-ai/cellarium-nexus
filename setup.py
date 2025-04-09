47b2d450eb23519f471d722bab127e7e32f02f05
fedorgrab <fvgrab@gmail.com>
2025-04-09 16:34:07 -0300

    Feat: Add setup.py for package configuration

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="cellarium-nexus",
+    version="0.1.0",
+    packages=find_packages(),
+    install_requires=[
+        # Your requirements will be read from requirements/backend.txt
+    ],
+)
