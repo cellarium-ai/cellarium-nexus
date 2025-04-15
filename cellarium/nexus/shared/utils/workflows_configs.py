a133557f26929ddb8943f0149b318fa4bf9b11b1
fedorgrab <fvgrab@gmail.com>
2025-04-15 15:21:37 -0300

    Fix(workflows_configs): Resolve trailing slash issue
    Ensure prefixes end with a single trailing slash consistently.

--- cellarium/nexus/shared/utils/workflows_configs.py
+++ cellarium/nexus/shared/utils/workflows_configs.py
@@ -64,8 +64,7 @@ def dump_configs_to_bucket(
     bucket_name, prefix = utils.gcp.get_bucket_name_and_file_path_from_gc_path(bucket_path)
     
     if prefix:
-        if not prefix.endswith("/"):
-            prefix += "/"
+        prefix = prefix.rstrip("/") + "/"
 
     with tempfile.TemporaryDirectory() as temp_dir:
         local_file_paths = []
